9789be5ddf9c63153254a6b88f3ee6e91249fa6d
Jonas Planck <jonas.planck@web.de>
2024-01-23 18:34:32 +0100

    the fuck

--- AutoCommit/Projekt/main_script.py
+++ AutoCommit/Projekt/main_script.py
@@ -1,12 +1,13 @@
-# main_script.py
-# from utils import initialize_driver, navigate_to_url, click_element
+import os
+
+
 from utils import open_browser, perform_login, ask_chat_gpt, clickOnCopy
 from idea_manager import IdeaManager
 from clipboard_extractor import ClipboardExtractor
 
 if __name__ == "__main__":
     # Benutzerdefinierter Dateipfad für IdeaManager
-    custom_file_path = r"C:\Users\jpl\VS Code for Git\codespaces-jupyter\(1) autoCommitGit\ideas.txt"
+    custom_file_path = os.path.abspath("ideas.txt")
     
     url = 'https://chat.openai.com/auth/login'
     open_browser(url)
